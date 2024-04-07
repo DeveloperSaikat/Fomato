ffdbda0ba8cf1370f86a20fe5789665e95ac390b
DeveloperSaikat <saikatk594@gmail.com>
2024-04-07 20:33:11 +0530

    minute changes

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
         </div>
         <div>
           <div className={classes.hero}>
-            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <h1>Delicious Food recipes for all the Foodies</h1>
             <p>Taste & share food from all over the world.</p>
           </div>
           <div className={classes.cta}>
@@ -25,25 +25,25 @@ export default function Home() {
         <section className={classes.section}>
           <h2>How it works</h2>
           <p>
-            NextLevel Food is a platform for foodies to share their favorite
+            Fomato Food is a platform for foodies to share their favorite
             recipes with the world. It&apos;s a place to discover new dishes, and to
             connect with other food lovers.
           </p>
           <p>
-            NextLevel Food is a place to discover new dishes, and to connect
+            Fomato Food is a place to discover new dishes, and to connect
             with other food lovers.
           </p>
         </section>
 
         <section className={classes.section}>
-          <h2>Why NextLevel Food?</h2>
+          <h2>Why Fomato Food?</h2>
           <p>
-            NextLevel Food is a platform for foodies to share their favorite
+            Fomato Food is a platform for foodies to share their favorite
             recipes with the world. It&apos;s a place to discover new dishes, and to
             connect with other food lovers.
           </p>
           <p>
-            NextLevel Food is a place to discover new dishes, and to connect
+            Fomato Food is a place to discover new dishes, and to connect
             with other food lovers.
           </p>
         </section>
